742e43bdc6f59e8b8b5975880a25eba5832fd4a4
tareqmonwer <tareqmonwer137@gmail.com>
2020-08-25 00:23:16 +0600

    Product, Category customized admin integration

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name', )}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price',
+                    'available', 'created_at', 'created_at']
+    list_filter = ['available', 'created_at', 'created_at']
+    list_editable = ['price', 'available']
+    prepopulated_fields = {'slug': ('name', )}
+
