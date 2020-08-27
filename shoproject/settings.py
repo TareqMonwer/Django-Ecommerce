4e53c5307bd32f98a6c69a120abf5d522e2a9ddf
tareqmonwer <tareqmonwer137@gmail.com>
2020-08-28 04:40:11 +0600

    cart:custom context_processor along with template

--- shoproject/settings.py
+++ shoproject/settings.py
@@ -66,6 +66,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # Custom Context Processors
+                'cart.context_processors.cart',
             ],
         },
     },
