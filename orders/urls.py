33877756808b087a3b45c46cea29e062288918e6
tareqmonwer <tareqmonwer137@gmail.com>
2020-09-01 00:18:46 +0600

    place order view, form, template and route

---
+++ orders/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+app_name = 'orders'
+urlpatterns = [
+    path('create', views.order_create, name='create_order'),
+]
--- shoproject/urls.py
+++ shoproject/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
+    path('orders/', include('orders.urls', namespace='orders')),
     path('', include('shop.urls', namespace='shop')),
 ]
 
