bd1a741f43b2bcdcb33dc64534cbc44d69fc7fab
tareqmonwer <tareqmonwer137@gmail.com>
2020-08-31 15:00:39 +0600

    Add orders in admin dashboard

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product', ]
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'email',
+        'address', 'postal_code', 'city', 'created', 'updated', 'paid']
+    list_filter = ['created', 'updated']
+    inlines = [OrderItemInline]
