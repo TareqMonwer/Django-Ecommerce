7f89c7f043399b7df6480b1f246fcaf82bcb3fef
tareqmonwer <tareqmonwer137@gmail.com>
2020-09-04 23:18:19 +0600

    :bug: OrderItem doesn't get saved after first cart item

--- orders/views.py
+++ orders/views.py
@@ -17,16 +17,16 @@ def order_create(request):
                                         product=item['product'],
                                         price=item['price'],
                                         quantity=item['quantity'])
-                # Clear the cart
-                cart.clear()
-                # Launch asynchronous task
-                order_created.delay(order.id)
-                # Set the order ID in the session
-                request.session['order_id'] = order.id
-                # Redirect for payment
-                return redirect(reverse('payment:process'))
-                # return render(request, 'orders/order/created.html',
-                #     {'order': order})
+            # Clear the cart
+            cart.clear()
+            # Launch asynchronous task
+            order_created.delay(order.id)
+            # Set the order ID in the session
+            request.session['order_id'] = order.id
+            # Redirect for payment
+            return redirect(reverse('payment:process'))
+            # return render(request, 'orders/order/created.html',
+            #     {'order': order})
     else:
         form = OrderCreateForm()
     return render(request, 'orders/order/create.html', 
