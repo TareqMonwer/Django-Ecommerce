5a6f76e3e9d647928d6d851b9b38bb0acc630548
tareqmonwer <tareqmonwer137@gmail.com>
2020-09-06 20:53:54 +0600

    payment_completed async task added

--- payment/views.py
+++ payment/views.py
@@ -2,6 +2,7 @@ import braintree
 from django.shortcuts import render, redirect, get_object_or_404
 from django.conf import settings
 from orders.models import Order
+from .tasks import payment_completed
 
 # Instantiate payment gateway (Braintree)
 gateway = braintree.BraintreeGateway(settings.BRAINTREE_CONF)
@@ -28,6 +29,8 @@ def payment_process(request):
             # Store the unique transaction id
             order.braintree_id = result.transaction.id
             order.save()
+            # launch asynchronous task
+            payment_completed.delay(order.id)
             return redirect('payment:done')
         else:
             return redirect('payment:canceled')
